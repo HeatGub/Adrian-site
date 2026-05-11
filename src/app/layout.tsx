1dd8c78524f7495645eeb03b86e638bf7c500bfb
heatgub <ptrchm@proton.me>
2026-05-11 23:48:56 +0200

    Add google verification tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,11 @@
 import "./globals.css";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  verification: {
+    google: "zwf1YlA4bFdcIEcF8qd_v8v3Dulxfv4d2ZnlIotzhi0",
+  },
+};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return children;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,38 +3,3 @@ import { redirect } from "next/navigation";
 export default function Home() {
   redirect("/en");
 }
-
-// import { NextResponse } from "next/server";
-// import type { NextRequest } from "next/server";
-
-// export function middleware(request: NextRequest) {
-//   const { pathname } = request.nextUrl;
-
-//   // nie ruszaj jeśli już ma język
-//   if (pathname.startsWith("/pl") || pathname.startsWith("/en")) {
-//     return NextResponse.next();
-//   }
-
-//   // cookie preferencji
-//   const langCookie = request.cookies.get("lang")?.value;
-
-//   if (langCookie === "en") {
-//     return NextResponse.redirect(new URL("/en", request.url));
-//   }
-
-//   if (langCookie === "pl") {
-//     return NextResponse.redirect(new URL("/pl", request.url));
-//   }
-
-//   // fallback: język przeglądarki
-//   const acceptLang = request.headers.get("accept-language");
-
-//   if (acceptLang?.startsWith("en")) {
-//     return NextResponse.redirect(new URL("/en", request.url));
-//   }
-
-//   // default
-//   return NextResponse.redirect(new URL("/pl", request.url));
-// }
-
-// document.cookie = "lang=en; path=/; max-age=31536000";
